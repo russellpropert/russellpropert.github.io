6451114de0113fff4700984adb8a5d2e91116ff9
Russell Propert <russellpropert@gmail.com>
2021-06-10 18:24:19 -0400

    Adding NextTech assignment.

---
+++ MIT-MERN/09week/NextTech_Working_With_The_DOM_To_Add_Map_Markers/mapmarker.js
@@ -0,0 +1,26 @@
+function createMapMarker() {
+  // TODO: add your own access token
+  mapboxgl.accessToken = 'pk.eyJ1IjoicnVzc2VsbHByb3BlcnQiLCJhIjoiY2twcmN6aHNoMDBycjJ2bzRkY2tzYXc0bSJ9.FtaHBAoVWf0xfdn8wAu6mw';
+
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.091542, 42.358862],
+    zoom: 12,
+  });
+
+  // TODO: add a marker to the map
+  const mapMarker = new mapboxgl.Marker()
+    .setLngLat([-71.091542, 42.358862])
+    .addTo(map);
+}
+
+// Do not edit the code below
+window.onload = () => {
+  createMapMarker();
+};
+
+// Do not edit code past this point
+if (typeof module !== 'undefined') {
+  module.exports = { createMapMarker };
+}
