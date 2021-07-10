1fd9d3d6d2ad215aa51c96103ac3b420eab3d485
Russell Propert <russellpropert@gmail.com>
2021-07-10 16:16:10 -0400

    Completing the word count warmup exercises.

--- MIT-MERN/11week/starter files/word-count/warmup.js
+++ MIT-MERN/11week/starter files/word-count/warmup.js
@@ -1,16 +1,41 @@
+console.log('Arrays and Objects');
 // clone the following array
 let a = [1, 2, 3, 4];
 // answer
 let b = [...a];
+console.log(b);
 
 // clone an object
-let a = { a: 1, b: 2 };
+let c = { a: 1, b: 2 };
 // answer
-let b = { ...a };
+let d = { ...c };
+console.log(d);
+
 // clone first n properties of an object
+let obj = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10};
+const firstProperties = (object, n) => {
+  let newObj = {};
+  for (let i = 0; i < n; i++) {
+    let key = Object.keys(object)[i];
+    newObj[key] = object[key];
+  }
+
+  return newObj;
+}
+console.log(firstProperties(obj, 8));
 
 //create a function that returns an array n long with numbers 1 to n
-const setArray = (n) => {
-  return result;
-};
+const setArray = (n) => Array.from({length: n}, (a, i) => i + 1);
+console.log(setArray(10));
+
 // create a function to sum every other element of an array with
+const addEveryOther = (array) => {
+  let sum = 0
+  for (let i = 0; i < array.length; i += 2) {
+    sum += array[i];
+  }
+
+  return sum;
+}
+
+console.log(addEveryOther(setArray(7)));
