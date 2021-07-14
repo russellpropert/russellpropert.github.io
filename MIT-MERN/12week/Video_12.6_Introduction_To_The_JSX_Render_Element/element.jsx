bfcb3034de6293f6448dbcd766f5335ade205e83
Russell Propert <russellpropert@gmail.com>
2021-07-13 22:36:04 -0400

    Adding file.

---
+++ MIT-MERN/12week/Video_12.6_Introduction_To_The_JSX_Render_Element/element.jsx
@@ -0,0 +1,9 @@
+const a = 'Nausicaä';
+const element = <h1>Hello {a}.</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
+// );
