bef59cd248d9d9282fccc72de5874a83e9d5bac4
Russell Propert <russellpropert@gmail.com>
2021-07-13 23:47:57 -0400

    Adding files.

---
+++ MIT-MERN/12week/Video_12.8_Passing_Properties_Into_A_Component/element.jsx
@@ -0,0 +1,16 @@
+const Hello = (a) => {
+  let color = a.action();
+  return (<h1 style={{backgroundColor: color}}>Hello {a.name} of the {a.place}.</h1>);
+};
+
+const getRandomColor = () => {
+  const palet = ["red", "green", "blue"];
+  let color = palet[Math.floor(Math.random() * 3)];
+  console.log(color);
+  return color;
+}
+
+ReactDOM.render(
+  <Hello name="Nausicaä" place="Valley of the Wind" action={getRandomColor}/>, 
+  document.getElementById('root')
+);
