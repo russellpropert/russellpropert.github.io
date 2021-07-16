50170c2c71cdcb2c1b30f991301e3f2f709a673c
Russell Propert <russellpropert@gmail.com>
2021-07-16 09:27:27 -0400

    Adding files.

---
+++ MIT-MERN/12week/NextTech_Destructuring_And_Props/destruct.jsx
@@ -0,0 +1,47 @@
+"use strict";
+
+class Person extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    details: {
+      name: "Maradona",
+      residence: {
+        city: "Tigre",
+        country: "Argentina",
+      },
+      profession: "soccer",
+    },
+  };
+
+  render() {
+    return <Details person={this.state} />;
+  }
+}
+
+const Details = ({person}) => {
+  // TODO: destructure props
+const {
+  details: {
+    name: name,
+    residence: {
+      city: city,
+      country: country
+    },
+    profession: profession
+  }
+} = person;
+
+  return (
+    <div style={{ margin: "1em" }}>
+      <div id="name">Name: {name}</div>
+      <div id="city">City: {city}</div>
+      <div id="country">Country: {country}</div>
+      <div id="profession">Profession: {profession}</div>
+    </div>
+  );
+};
+
+ReactDOM.render(React.createElement(Person), document.getElementById("root"));
