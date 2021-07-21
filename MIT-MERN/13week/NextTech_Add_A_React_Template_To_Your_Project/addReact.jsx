d489cd014f8200a9fa9975b07cbbde6a1bf7f42e
Russell Propert <russellpropert@gmail.com>
2021-07-21 08:14:47 -0400

    Dividing last line of code for clarity.

--- MIT-MERN/13week/NextTech_Add_A_React_Template_To_Your_Project/addReact.jsx
+++ MIT-MERN/13week/NextTech_Add_A_React_Template_To_Your_Project/addReact.jsx
@@ -21,4 +21,6 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');ReactDOM.render(e(LikeButton), domContainer);
+const domContainer = document.querySelector('#like_button_container');
+
+ReactDOM.render(e(LikeButton), domContainer);
