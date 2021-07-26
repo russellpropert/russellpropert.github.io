08944b051fb0fb1aacb7423b5bef1875dc72ce1d
Russell Propert <russellpropert@gmail.com>
2021-07-25 21:58:26 -0400

    Removing unneccessary condition from onSubmit alert and adding line break for the button.

--- MIT-MERN/13week/NextTech_Build_A_Formik_Form/App.js
+++ MIT-MERN/13week/NextTech_Build_A_Formik_Form/App.js
@@ -10,9 +10,7 @@ function App() {
       password: ''
     },
     onSubmit: values => {
-      if (!formik.errors.email && !formik.errors.password) {
         alert('Login Successful');
-      }
     },
     validate: values => {
       const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -25,16 +23,13 @@ function App() {
   });
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
       <form onSubmit={formik.handleSubmit}>
         <div>Email</div>
         <input id="emailField" name="email" type="text" onChange={formik.handleChange} value={formik.values.email} />
         {formik.errors.email ? <div id="emailError" style={{color: 'red'}}>{formik.errors.email}</div> : null}
         <div>Password</div>
         <input id="pswField" name="password" type="text" onChange={formik.handleChange} value={formik.values.password} />
+        <br />
         {formik.errors.password ? <div id="pswError" style={{color: 'red'}}>{formik.errors.password}</div> : null}
         <button id="submitBtn" type="submit">Submit</button>
       </form>
