d885665407b980246e4761b1fc30f3755aa9d79d
Russell Propert <russellpropert@gmail.com>
2021-08-25 12:23:48 -0400

    Adding files.

---
+++ MIT-MERN/17week/Video_17-6_Create_Web_Components_Programmatically/books.js
@@ -0,0 +1,45 @@
+class Book extends HTMLElement{
+  constructor(){
+    super();
+  
+  }
+
+  set bookTest(bookObject) {
+
+    this.innerHTML = `
+      <div class="card">
+        <h5 class="card-header">${bookObject.title}</h5>
+        <div class="card-body">
+          <h5 class="card-title">${bookObject.subtitle}</h5>
+          <p class="card-text">
+            <table class="table">
+              <tr>
+                <td class="text-success font-weight-bold">Title:</td>
+                <td>${bookObject.title}</td>
+              </tr>
+              <tr>
+                <td class="text-success font-weight-bold">Subtitle:</td>
+                <td>${bookObject.subtitle}</td>
+              </tr>
+              <tr>
+                <td class="text-success font-weight-bold">Author:</td>
+                <td>${bookObject.author}</td>
+              </tr>
+              <tr>
+                <td class="text-success font-weight-bold">Publisher:</td>
+                <td>${bookObject.publisher}</td>
+              </tr>
+              <tr>
+                <td class="text-success font-weight-bold">Description:</td>
+                <td>${bookObject.description}</td>
+              </tr>
+            </table>
+          </p>
+        </div>
+      </div>  
+    `;
+
+  }
+}
+
+customElements.define('mit-book', Book);
---
+++ MIT-MERN/17week/Video_17-6_Create_Web_Components_Programmatically/main.js
@@ -0,0 +1,19 @@
+import './books.js';
+
+window.addEventListener('load', () => {
+  createBookList();
+});
+
+async function createBookList() {
+  const response = await fetch('./books.json');
+  const json = await response.json();
+  const books = document.getElementById('books');
+
+  json.books.forEach((book) => {
+    const element = document.createElement('mit-book');
+    console.log(element);
+    element.bookTest = book;
+    books.appendChild(element);
+  });
+
+}
