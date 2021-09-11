328fb30e79be5b311bbb912519abdaa34be4c5ec
Russell Propert <russellpropert@gmail.com>
2021-09-11 14:47:51 -0400

    Adding React app src.

---
+++ MIT-MERN/20week/Video_20-3_Create_React_App_Shopping_Cart_Example/src/App.js
@@ -0,0 +1,9 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Products from './Components/products.jsx'
+
+function App() {
+  return <Products></Products>
+}
+
+export default App;
