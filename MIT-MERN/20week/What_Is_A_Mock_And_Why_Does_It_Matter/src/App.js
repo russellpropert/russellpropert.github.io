863b1abc973ccba9a7a6d224265c06a3d6802d5f
Russell Propert <russellpropert@gmail.com>
2021-09-17 10:28:59 -0400

    Adding files.

---
+++ MIT-MERN/20week/What_Is_A_Mock_And_Why_Does_It_Matter/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>Look in these files</div>
+      <br />
+      <div>src/request.js</div>
+      <div>src/user.js</div>
+      <div>src/__mocks__/request.js</div>
+      <div>src/__test__/user-test.js</div>
+    </div>
+  );
+}
+
+export default App;
