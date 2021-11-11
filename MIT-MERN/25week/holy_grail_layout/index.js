0ba2929ab229d5d118e4217392ab31cd70255592
Russell Propert <russellpropert@gmail.com>
2021-11-10 19:11:38 -0500

    Moving front end to public folder and creating node express server.

--- MIT-MERN/25week/holy_grail_layout/index.js
+++ MIT-MERN/25week/holy_grail_layout/index.js
@@ -1,57 +1,13 @@
-function PlusMinus(props) {
-  function runHandle(value) {
-    props.handle({section: props.section, value});
-  }
-  
-  return (
-    <>
-      <img src={`icons/${props.section}_plus.png`} alt="Plus button" onClick={() => runHandle(1)} />
-      <img src={`icons/${props.section}_minus.png`} alt="Minus button" onClick={() => runHandle(-1)} />
-    </>
-  );
-}
+import express from 'express';
 
-function Data(props) {
-  
-  return (
-    props.section === 'header' || props.section === 'footer' ?
-      <div>
-        Header: {props.data.header}, 
-        Left: {props.data.left}, 
-        Article: {props.data.article}, 
-        Right: {props.data.right}, 
-        Footer: {props.data.footer}
-      </div>
-    :
-      <div>
-        Header: {props.data.header}<br/>
-        Left: {props.data.left}<br/>
-        Article: {props.data.article}<br/>
-        Right: {props.data.right}<br/>
-        Footer: {props.data.footer}
-      </div>
-  );
-}
+const app = express();
 
-function App() {
-  const [data, setData] = React.useState({header: 0, left: 0, article: 0, right: 0, footer: 0});
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-  function handle(update) {
-    console.log('Update:', update);
-    const newValue = data[update.section] + update.value;
-    const object = {[update.section]: newValue};
-    setData({...data, ...object});
-  }
+const port = 4000;
 
-  return (
-    <div className="grid">
-      <Header handle={handle} data={data}/>
-      <Left handle={handle} data={data}/>
-      <Article handle={handle} data={data}/>
-      <Right handle={handle} data={data}/>
-      <Footer handle={handle} data={data}/>
-    </div>
-  );
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
